a48a421d89478d17850fc9cc1952f879ecb1b507
vinceybanez5521 <vinceybanez5521@gmail.com>
2024-04-06 16:37:49 +0800

    Connected route to controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    // Common Controller Functions: index, show, store, update, destroy
+    public function index() {
+        return '<h1>UserController Index Page</h1>';
+    }
+
+    public function show($id) {
+        /* // Query Parameter
+        $search = request()->query('search');
+        dd($search); */
+
+        return '<h1>UserController Show Page ' . $id . '</h1>';
+    }
+}
