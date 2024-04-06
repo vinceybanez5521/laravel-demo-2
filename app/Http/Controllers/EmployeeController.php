f3c16e0e604813d1b5235c87e1895401175690d3
vinceybanez5521 <vinceybanez5521@gmail.com>
2024-04-06 22:48:38 +0800

    Created new migration file for adding column age and make it nullable

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -7,15 +7,19 @@ use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    public function index() {
+    public function index()
+    {
         $data = Employee::all();
         // dd($data) ;
 
         return view('employee.index', ['employees' => $data]);
     }
 
-    public function show($id) {
+    public function show($id)
+    {
+        $data = Employee::where('id', $id)->get()->first();
         $data = Employee::where('id', $id)->first();
+        $data = Employee::find($id);
         $data = Employee::findOrFail($id);
         // dd($data);
 
