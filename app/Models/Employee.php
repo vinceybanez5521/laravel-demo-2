316672f54bc596b99f7805153cc857d4a33dc73f
vinceybanez5521 <vinceybanez5521@gmail.com>
2024-04-08 21:13:06 +0800

    Added more Model Attributes in Employee model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -15,4 +15,23 @@ class Employee extends Model
         'gender',
         'email',
     ];
+
+    /* 
+    protected $guarded = [
+        'created_at',
+        'updated_at'
+    ]; 
+    */
+
+    // protected $guarded = []; // will make all fields fillable
+
+    // commonly used with APIs - used to hide fields from API request
+    // fields inside this attribute will not be returned in the API response
+    /* protected $hidden = [
+        'id',
+        'created_at',
+        'updated_at'
+    ]; */
+
+    // protected $table = 'legacy_employees'; // specify which table this object should point to
 }
